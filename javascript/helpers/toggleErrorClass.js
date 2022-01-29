5ca6c4c03797e5bbd21caea89756c6b41148af42
Mateusz Bogacz <shay_am@wp.pl>
2022-01-29 13:36:48 +0100

    added type toggle class

--- javascript/helpers/toggleErrorClass.js
+++ javascript/helpers/toggleErrorClass.js
@@ -1,4 +1,4 @@
-export const toggleErrorClass = (type, input, description) => {
+export const toggleErrorClass = (type = "toggle", input, description) => {
 	if (typeof input !== "undefined" && typeof description !== "undefined") {
 		input.classList[type]("error");
 		description.classList[type]("error-code");
