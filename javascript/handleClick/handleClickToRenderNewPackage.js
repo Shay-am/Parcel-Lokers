a88b5b2e926150ab18cbcf7ed254a4b90eab61dc
Mateusz Bogacz <shay_am@wp.pl>
2022-01-29 13:50:49 +0100

    fixed bug with validation

--- javascript/handleClick/handleClickToRenderNewPackage.js
+++ javascript/handleClick/handleClickToRenderNewPackage.js
@@ -1,7 +1,9 @@
-import { modalWrapper, container } from "../helpers/DomElements.js";
+import { modalWrapper, container, button } from "../helpers/DomElements.js";
 import { resetInputs } from "../helpers/resetValueInput.js";
+import { checkIsValidInputs } from "../validation/checkIsValidInputs.js";
 
 export const handleRedirectToNewPackage = () => {
+	checkIsValidInputs();
 	modalWrapper.classList.remove("active");
 	container.classList.remove("opacity");
 	resetInputs();
